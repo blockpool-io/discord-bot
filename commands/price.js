77f39e8aa4d76e67b8af76166eee042a8fd4640b
dated <hello@dated.fun>
2018-10-15 12:50:05 +0200

    fixes coin name

--- commands/price.js
+++ commands/price.js
@@ -11,7 +11,7 @@ exports.run = (client, config, message, params) => {
       message.channel.send({
         embed: {
           color: 3977445,
-          title: `Latest ${data.Data.CoinInfo.Name} price from CryptoCompare:`,
+          title: `Latest ${data.Data.CoinInfo.FullName} price from CryptoCompare:`,
           fields: [{
             name: "Price (BTC)",
             value: Number(data.Data.AggregatedData.PRICE).toFixed(8).toString()
